a0870ccc178caa0a0d24cdb3dfc799e3efb48298
Siddhu054 <siddharthaballa123@gmail.com>
2024-12-26 12:36:20 +0530

    Add files via upload

---
+++ devhub-frontend/next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:7000/api/:path*",
+      },
+    ];
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
